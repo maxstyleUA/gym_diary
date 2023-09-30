8a71f78d0c56d9c3599477e9dca6c5d57d30445c
maxstyleUA <kashlyatsky@gmail.com>
2023-09-30 23:42:23 +0300

    feat: added file srtucture to project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,49 @@
 module.exports = {
-    'env': {
-        es6: true,
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  overrides: [
+    {
+      env: {
         node: true,
-        jest: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    ],
-    'overrides': [
-        {
-            'env': {
-                'node': true
-            },
-            'files': [
-                '.eslintrc.{js,cjs}'
-            ],
-            'parserOptions': {
-                'sourceType': 'script'
-            }
-        }
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        project: './tsconfig.json',
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'linebreak-style': ['error', 'unix'],
+    semi: ['error', 'never'],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    'plugins': ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
-    'rules': {
-        indent: ['error', 2, {SwitchCase: 1}],
-        quotes: ['error', 'single', {avoidEscape: true}],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ],
-        'no-empty-function': 'off',
-        '@typescript-eslint/no-empty-function': 'off',
-        'react/display-name': 'off',
-        'react/prop-types': 'off',
-        'prettier/prettier': 'error',
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
+  },
 }
